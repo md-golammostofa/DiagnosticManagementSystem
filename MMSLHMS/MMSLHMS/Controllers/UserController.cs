9b934c262fb0ea09f596c31f77a1c06611605661
Md. Golam Mostofa <58271557+md-golammostofa@users.noreply.github.com>
2021-09-14 11:34:27 +0600

    Commit_2_Mostofa_14_09_2021_DMS

---
+++ MMSLHMS/MMSLHMS/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using MMSLHMS.DAL;
+using MMSLHMS.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMSLHMS.Controllers
+{
+    [CustomAuthorize]
+    public class UserController : BaseController
+    {
+        // GET: User
+        private DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            ViewBag.TotalAmountThisMonth = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId && bi.EntryDate.Value.Month == DateTime.Now.Month && bi.EntryDate.Value.Year == DateTime.Now.Year).Sum(i => i.NetAmount).ToString();
+            ViewBag.TotalInvestigationThisMonth = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId && bi.EntryDate.Value.Month == DateTime.Now.Month && bi.EntryDate.Value.Year == DateTime.Now.Year).Sum(i => i.InvestigationQty).ToString();
+
+            ViewBag.TotalAmountThisYear = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId && bi.EntryDate.Value.Year == DateTime.Now.Year).Sum(i => i.NetAmount).ToString();
+            ViewBag.TotalInvestigationThisYear = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId  && bi.EntryDate.Value.Year == DateTime.Now.Year).Sum(i => i.InvestigationQty).ToString();
+
+            ViewBag.TotalInvestigationToday = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId && DbFunctions.TruncateTime(bi.EntryDate.Value) == DbFunctions.TruncateTime(DateTime.Today)).Sum(i => i.InvestigationQty).ToString();
+            ViewBag.TotalAmountToday = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId && DbFunctions.TruncateTime(bi.EntryDate.Value) == DbFunctions.TruncateTime(DateTime.Today)).Sum(i => i.NetAmount).ToString();
+
+            ViewBag.TotalDueAmountTillNow = db.tblDiagnosticBillInfo.Where(bi => bi.OrgId == User.OrgId).Sum(i => i.DueAmount).ToString();
+
+            return View();
+        }
+    }
+}
